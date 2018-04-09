f4b93fe3ee6efe91c20bdd4e397ad924e3ab223e
Pushss <chrisjamescam10@gmail.com>
2018-04-09 23:35:03 +0100

    nav_bar added for home and about pages

--- script1.py
+++ script1.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__) #instantiate objec/create instance(will get name of python script)
+app=Flask(__name__) #instantiate object/create instance(will get name of python script)
 
 @app.route('/')#Decorator(sets home)
 def home():
-    return "Website homepage!" #returns text
+    return render_template("home.html") #returns text
 
 @app.route('/about/')#Decorator(sets about)
 def about():
-    return "This is the about page!" #returns text
+    return render_template("about.html") #returns text
 
 if __name__ == "__main__": #if script1.py equals __main__ then run.
     app.run(debug=True)#runs app
